6785bc53a0add1cd3027b3b4eb424a8fbf10cd3b
Gioele Cavallo <gioele.cavallo@CPT.local>
2022-09-21 11:32:22 +0200

    vagrant with 2 vm: db and web

--- scripts/provision_mysqldb.sh
+++ scripts/provision_mysqldb.sh
@@ -6,6 +6,6 @@ DBPASSWD=vagrantpass
 
 echo "Creating new db $DBNAME"
 sudo mysql -u root -e "CREATE DATABASE $DBNAME"
-sudo mysql -u root -e "CREATE TABLE PROVA(ID INT PRIMARY KEY);"
+sudo mysql -u root -e "USE $DBNAME; CREATE TABLE PROVA(ID INT PRIMARY KEY);"
 sudo mysql -u root -e "CREATE USER '$DBUSER'@'%' IDENTIFIED BY '$DBPASSWD';"
 sudo mysql -u root -e "grant all privileges on $DBNAME.* to '$DBUSER'@'%'"
